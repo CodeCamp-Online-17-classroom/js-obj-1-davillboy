67c8850b29e41153bf48d2103237c90acef7f051
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 14:16:30 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // write code here
+<script>
+        let person = {
+            name: 'Chairat',
+            age: 32,
+            gender: 'Male',
+            address: '73/1 Samutsakhon',
+            phoneNumber: '08611111'
+        };
+        console.log(person)
+    </script>
